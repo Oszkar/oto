cc50ee16cdb925f848e1eec16756c954aeb28680
Oszkar <mail@oszkar.me>
2026-06-30 19:02:29 +0900

    feat: add home view state provider

---
+++ app/lib/src/state/home_view_state.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_view_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(homeViewState)
+final homeViewStateProvider = HomeViewStateProvider._();
+
+final class HomeViewStateProvider
+    extends $FunctionalProvider<HomeViewState, HomeViewState, HomeViewState>
+    with $Provider<HomeViewState> {
+  HomeViewStateProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'homeViewStateProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$homeViewStateHash();
+
+  @$internal
+  @override
+  $ProviderElement<HomeViewState> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  HomeViewState create(Ref ref) {
+    return homeViewState(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(HomeViewState value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<HomeViewState>(value),
+    );
+  }
+}
+
+String _$homeViewStateHash() => r'871ddc2f5f3650b9f7d9f138ce8dd40ffb71f34f';
